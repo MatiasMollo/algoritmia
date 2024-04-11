f1b629757f19ee10b3d2bef02a61763555b1c904
Matias Mollo <mollo.matias03@gmail.com>
2024-04-11 00:50:31 -0300

    Ej 12 Fibonacci

--- 12.py
+++ 12.py
@@ -6,10 +6,12 @@ limite = int(input("Ingrese el número limite de la sucesion de Fibonacci: "))
 
 a = 0
 b = 1
+suma = 0
 cont = 0
 
 while cont <= limite:
-    print(a)
-    a = a+b
-    b = a
+    print(suma)
+    a = b
+    b = suma
+    suma = a + b
     cont += 1
